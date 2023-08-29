5b14d69bf00b572ed2b2ee23d7016cba62055052
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-29 18:42:03 +0000

    Javascript

---
+++ js/number.js
@@ -0,0 +1,19 @@
+{
+    var slice_of_pizza = 10;
+    console.log(slice_of_pizza % 3);
+
+    var precendence1 = 5 + 3 * 12 -20 /10;
+    var precendence2 = (5 + 3) * (12 -20) /10;
+    console.log(precendence1);
+    console.log(precendence2);
+    /*
+
+
+    +
+    -
+    *
+    /
+    %
+
+    */
+}
