fecf81a910c910713812bfc151766771103006a4
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-29 18:26:38 +0000

    Javascript

---
+++ js/data-types.js
@@ -0,0 +1,23 @@
+{
+    let x = 5;
+    let b = '10';
+    x += b; 
+    console.log(x)
+}
+
+let person = {
+    name: 'John',
+    age: 21,
+    favFood: "Pizza",
+    fun: function(){
+        console.log("Yayyyy");
+    }
+}
+person.age;
+person.fun()
+
+{
+    let myName = "Caleb";
+    myName = myName.toUpperCase();
+    console.log(myName);
+}
--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,14 @@
   //IIFE - Immediately invoked function expression
   var age = 5;  
 })();
+{
+  //block
+  let y = 10;
+  const x =20;
+  {
+    let y = 10;
+    console.log(y);
+  }
+
+  console.log(x, y);
+}
